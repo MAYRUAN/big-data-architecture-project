172a0c9fe3e32d7bbe94d179fc9b8521196afdd6
May Ruan <maymayr1130@gmail.com>
2026-08-22 18:52:07 -0500

    Update create_tables.sql

--- dsc650-big-data-portfolio-starter-main/hive/create_tables.sql
+++ dsc650-big-data-portfolio-starter-main/hive/create_tables.sql
@@ -6,7 +6,7 @@
 --   2. Add short comments explaining important tables.
 --   3. Keep the SQL that best demonstrates your work.
 
--- Example structure only:
+-- In the Hive CLI, create a table named branch_growth:
 CREATE TABLE branch_growth (
   `Branch_ID` STRING,
   `Region` STRING,
@@ -17,3 +17,7 @@ ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
+
+
+-- Load data into the Hive Managed table branch_growth:
+LOAD DATA INPATH '/tmp/branch_growth.csv' INTO TABLE branch_growth;
