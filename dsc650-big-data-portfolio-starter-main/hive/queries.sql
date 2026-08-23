ba175da833d25a9f888abcd5e7f239e0cdb1c547
May Ruan <maymayr1130@gmail.com>
2026-08-22 19:08:26 -0500

    Update queries.sql

--- dsc650-big-data-portfolio-starter-main/hive/queries.sql
+++ dsc650-big-data-portfolio-starter-main/hive/queries.sql
@@ -3,7 +3,8 @@
 --
 -- Choose queries that demonstrate meaningful work rather than every query you ran.
 
--- Example:
--- SELECT ...
--- FROM ...
--- WHERE ...;
+-- SQL query1:
+select * from branch_growth;
+
+-- SQL query2 with aggregation:
+select count(*) as total_branches, sum(Ad_Budget_K ) as total_ad_budget_k, sum(New_Accnts_Opened) AS total_new_accounts, AVG(New_Accnts_Opened) AS avg_accounts_per_branch from branch_growth;
